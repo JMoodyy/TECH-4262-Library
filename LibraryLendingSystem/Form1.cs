8fec66592b0294f11e4ab190fdb169f55038d38d
charris442 <cnichaelharris1@gmail.com>
2025-12-05 13:07:25 -0600

    Added Tabs

--- LibraryLendingSystem/Form1.cs
+++ LibraryLendingSystem/Form1.cs
@@ -16,5 +16,11 @@ namespace LibraryLendingSystem
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
