720b2ee0857f3b0baecf98eb5d1693861fb2ac8f
charris442 <cnichaelharris1@gmail.com>
2025-12-05 14:40:24 -0600

    Created a member.cs file with the Members section and books section from database Copy of database will give added soon

---
+++ LibraryLendingSystem/Members.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryLendingSystem
+{
+    public class Member
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+
+    }
+    public class Books
+    {
+        public int bookId { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string ISBN { get; set; }
+        public int availablebooks { get; set; }
+        public int totalbooks { get; set; }
+    }
+    internal class Members
+    {
+
+    }
+}
