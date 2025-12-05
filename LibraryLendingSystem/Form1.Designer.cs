a766ffa369f72c0266953b68200cb5ecd7119c4c
charris442 <cnichaelharris1@gmail.com>
2025-12-05 13:39:27 -0600

    Made the UI for member section Each part was named the following:
    Search label: Searchlbl
    Search box: Searchtxt
    Search Button: Searchbtn
    
    Name Label: Namelbl
    Name box: Nametxt
    
    Phone Label: Phonelbl
    Phone box: Phonetxt
    
    Email Label: Emaillbl
    Email box: Emailtxt
    
    New Member button: Newbtn
    Delete Member button: Deletebtn
    Update Info button: Updatebtn
    Clear Button: Clearbtn

--- LibraryLendingSystem/Form1.Designer.cs
+++ LibraryLendingSystem/Form1.Designer.cs
@@ -30,9 +30,26 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.Clearbtn = new System.Windows.Forms.Button();
+            this.Deletebtn = new System.Windows.Forms.Button();
+            this.Updatebtn = new System.Windows.Forms.Button();
+            this.Newbtn = new System.Windows.Forms.Button();
+            this.Emaillbl = new System.Windows.Forms.Label();
+            this.Emailtxt = new System.Windows.Forms.TextBox();
+            this.Phonelbl = new System.Windows.Forms.Label();
+            this.Phonetxt = new System.Windows.Forms.TextBox();
+            this.Namelbl = new System.Windows.Forms.Label();
+            this.Nametxt = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Searchbtn = new System.Windows.Forms.Button();
+            this.Searchlbl = new System.Windows.Forms.Label();
+            this.Searchtxt = new System.Windows.Forms.TextBox();
+            this.Memberslbl = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -43,25 +60,176 @@
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(700, 381);
+            this.tabControl1.Size = new System.Drawing.Size(788, 438);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
+            this.tabPage1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tabPage1.Controls.Add(this.Clearbtn);
+            this.tabPage1.Controls.Add(this.Deletebtn);
+            this.tabPage1.Controls.Add(this.Updatebtn);
+            this.tabPage1.Controls.Add(this.Newbtn);
+            this.tabPage1.Controls.Add(this.Emaillbl);
+            this.tabPage1.Controls.Add(this.Emailtxt);
+            this.tabPage1.Controls.Add(this.Phonelbl);
+            this.tabPage1.Controls.Add(this.Phonetxt);
+            this.tabPage1.Controls.Add(this.Namelbl);
+            this.tabPage1.Controls.Add(this.Nametxt);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.Searchbtn);
+            this.tabPage1.Controls.Add(this.Searchlbl);
+            this.tabPage1.Controls.Add(this.Searchtxt);
+            this.tabPage1.Controls.Add(this.Memberslbl);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(192, 74);
+            this.tabPage1.Size = new System.Drawing.Size(780, 412);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Members";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // Clearbtn
+            // 
+            this.Clearbtn.Location = new System.Drawing.Point(473, 195);
+            this.Clearbtn.Name = "Clearbtn";
+            this.Clearbtn.Size = new System.Drawing.Size(105, 23);
+            this.Clearbtn.TabIndex = 16;
+            this.Clearbtn.Text = "Clear ";
+            this.Clearbtn.UseVisualStyleBackColor = true;
+            // 
+            // Deletebtn
+            // 
+            this.Deletebtn.Location = new System.Drawing.Point(473, 154);
+            this.Deletebtn.Name = "Deletebtn";
+            this.Deletebtn.Size = new System.Drawing.Size(105, 23);
+            this.Deletebtn.TabIndex = 15;
+            this.Deletebtn.Text = "Delete Member";
+            this.Deletebtn.UseVisualStyleBackColor = true;
+            // 
+            // Updatebtn
+            // 
+            this.Updatebtn.Location = new System.Drawing.Point(328, 195);
+            this.Updatebtn.Name = "Updatebtn";
+            this.Updatebtn.Size = new System.Drawing.Size(105, 23);
+            this.Updatebtn.TabIndex = 14;
+            this.Updatebtn.Text = "Update Info";
+            this.Updatebtn.UseVisualStyleBackColor = true;
+            // 
+            // Newbtn
+            // 
+            this.Newbtn.Location = new System.Drawing.Point(328, 154);
+            this.Newbtn.Name = "Newbtn";
+            this.Newbtn.Size = new System.Drawing.Size(105, 23);
+            this.Newbtn.TabIndex = 12;
+            this.Newbtn.Text = "New Member";
+            this.Newbtn.UseVisualStyleBackColor = true;
+            // 
+            // Emaillbl
+            // 
+            this.Emaillbl.AutoSize = true;
+            this.Emaillbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Emaillbl.Location = new System.Drawing.Point(325, 111);
+            this.Emaillbl.Name = "Emaillbl";
+            this.Emaillbl.Size = new System.Drawing.Size(46, 17);
+            this.Emaillbl.TabIndex = 10;
+            this.Emaillbl.Text = "Email:";
+            // 
+            // Emailtxt
+            // 
+            this.Emailtxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Emailtxt.Location = new System.Drawing.Point(392, 108);
+            this.Emailtxt.Name = "Emailtxt";
+            this.Emailtxt.Size = new System.Drawing.Size(183, 20);
+            this.Emailtxt.TabIndex = 11;
+            // 
+            // Phonelbl
+            // 
+            this.Phonelbl.AutoSize = true;
+            this.Phonelbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Phonelbl.Location = new System.Drawing.Point(325, 81);
+            this.Phonelbl.Name = "Phonelbl";
+            this.Phonelbl.Size = new System.Drawing.Size(53, 17);
+            this.Phonelbl.TabIndex = 8;
+            this.Phonelbl.Text = "Phone:";
+            // 
+            // Phonetxt
+            // 
+            this.Phonetxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Phonetxt.Location = new System.Drawing.Point(392, 78);
+            this.Phonetxt.Name = "Phonetxt";
+            this.Phonetxt.Size = new System.Drawing.Size(183, 20);
+            this.Phonetxt.TabIndex = 9;
+            // 
+            // Namelbl
+            // 
+            this.Namelbl.AutoSize = true;
+            this.Namelbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Namelbl.Location = new System.Drawing.Point(325, 54);
+            this.Namelbl.Name = "Namelbl";
+            this.Namelbl.Size = new System.Drawing.Size(49, 17);
+            this.Namelbl.TabIndex = 6;
+            this.Namelbl.Text = "Name:";
+            // 
+            // Nametxt
+            // 
+            this.Nametxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Nametxt.Location = new System.Drawing.Point(392, 51);
+            this.Nametxt.Name = "Nametxt";
+            this.Nametxt.Size = new System.Drawing.Size(183, 20);
+            this.Nametxt.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(279, 300);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Searchbtn
+            // 
+            this.Searchbtn.Location = new System.Drawing.Point(179, 25);
+            this.Searchbtn.Name = "Searchbtn";
+            this.Searchbtn.Size = new System.Drawing.Size(75, 23);
+            this.Searchbtn.TabIndex = 4;
+            this.Searchbtn.Text = "Search";
+            this.Searchbtn.UseVisualStyleBackColor = true;
+            // 
+            // Searchlbl
+            // 
+            this.Searchlbl.AutoSize = true;
+            this.Searchlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Searchlbl.Location = new System.Drawing.Point(6, 31);
+            this.Searchlbl.Name = "Searchlbl";
+            this.Searchlbl.Size = new System.Drawing.Size(61, 17);
+            this.Searchlbl.TabIndex = 2;
+            this.Searchlbl.Text = "Search: ";
+            // 
+            // Searchtxt
+            // 
+            this.Searchtxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Searchtxt.Location = new System.Drawing.Point(73, 28);
+            this.Searchtxt.Name = "Searchtxt";
+            this.Searchtxt.Size = new System.Drawing.Size(100, 20);
+            this.Searchtxt.TabIndex = 3;
+            // 
+            // Memberslbl
+            // 
+            this.Memberslbl.AutoSize = true;
+            this.Memberslbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Memberslbl.Location = new System.Drawing.Point(3, 3);
+            this.Memberslbl.Name = "Memberslbl";
+            this.Memberslbl.Size = new System.Drawing.Size(94, 25);
+            this.Memberslbl.TabIndex = 0;
+            this.Memberslbl.Text = "Members";
             // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.Size = new System.Drawing.Size(780, 412);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Books";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -71,7 +239,7 @@
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(692, 355);
+            this.tabPage3.Size = new System.Drawing.Size(780, 412);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Loans";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -86,6 +254,9 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -96,6 +267,21 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label Memberslbl;
+        private System.Windows.Forms.TextBox Searchtxt;
+        private System.Windows.Forms.Label Searchlbl;
+        private System.Windows.Forms.Button Searchbtn;
+        private System.Windows.Forms.Label Emaillbl;
+        private System.Windows.Forms.TextBox Emailtxt;
+        private System.Windows.Forms.Label Phonelbl;
+        private System.Windows.Forms.TextBox Phonetxt;
+        private System.Windows.Forms.Label Namelbl;
+        private System.Windows.Forms.TextBox Nametxt;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Updatebtn;
+        private System.Windows.Forms.Button Newbtn;
+        private System.Windows.Forms.Button Clearbtn;
+        private System.Windows.Forms.Button Deletebtn;
     }
 }
 
